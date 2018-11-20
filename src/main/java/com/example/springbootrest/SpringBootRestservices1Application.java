eaa92681ff4c008526a37957423717cf207f4e3f
Sreelakshmi-N <sree25.lakshmi@gmail.com>
2018-11-20 11:49:36 -0500

    commit

---
+++ src/main/java/com/example/springbootrest/SpringBootRestservices1Application.java
@@ -0,0 +1,12 @@
+package com.example.springbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestservices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestservices1Application.class, args);
+	}
+}
